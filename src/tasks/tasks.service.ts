04f8f0d7814ae1b986e7acd30bc356eede41cbcf
Pierre Cruz <contact@pierrecruz.com>
2019-12-19 18:47:50 -0400

    Data persistence section DONE

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,6 +4,7 @@ import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -12,28 +13,9 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  /*getAllTasks(): Task[] {
-    return this.tasks;
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto);
   }
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      tasks = tasks.filter(task => task.status === status);
-    }
-
-    if (search) {
-      tasks = tasks.filter(task =>
-       task.title.includes(search) ||
-       task.description.includes(search),
-      );
-    }
-
-    return tasks;
-  }
-  */
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
@@ -49,14 +31,12 @@ export class TasksService {
     return this.taskRepository.createTask(createTaskDto);
   }
 
-  /*
-
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    const task = this.getTaskById(id);
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
     task.status = status;
+    await task.save();
     return task;
   }
-  */
 
   async deleteTaskById(id: number): Promise<void> {
     const result = await this.taskRepository.delete(id);
